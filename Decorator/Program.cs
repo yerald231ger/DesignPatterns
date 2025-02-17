fb624ee346f11fca95db37c499932804063c7189
yerald231ger <yerald231ger@gmail.com>
2025-02-17 13:07:15 -0600

    Add initial project files for Decorator pattern implementation

---
+++ Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Decorator;
+using Decorator.Pattern;
+
+IGasStationService gasStationService = new FuelDispense
+{
+    Product = FuelDispense.ProductType.Premium,
+    Volume = 35
+};
+
+Console.WriteLine($"Cost of fuel dispense service: {gasStationService.GetServiceCost()}");
+
+gasStationService = new GasolineAdditiveDecorator(gasStationService);
+gasStationService = new FilterChangeDecorator(gasStationService);
+
+Console.WriteLine($"Cost of fuel dispense service: {gasStationService.GetServiceCost()}");
