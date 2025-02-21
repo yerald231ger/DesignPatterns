5acb107f6dbc6db16176ac74dcf8e5202ac7e133
yerald231ger <yerald231ger@gmail.com>
2025-02-21 00:36:33 -0600

    Add Facade pattern implementation for tank inventory management

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,21 +1,28 @@
-﻿using Adapter;
-using Adapter.Devices;
+﻿using Adapter.Devices;
 using Adapter.Pattern;
 
-ITankInventoryAdapter fusionAdapter = new TankInventoryFusionAdapter(new FusionAdaptee());
-ITankInventoryAdapter fdcAdapter = new TankInventoryVeederRootAdapter(new VeederRootAdaptee());
+namespace Adapter;
 
-ValidateTankVolume(fusionAdapter);
-ValidateTankVolume(fdcAdapter);
-
-return;
-
-void ValidateTankVolume(ITankInventoryAdapter adapter)
+internal static class Program
 {
-    const int tankId = 1;
-    var inventory = adapter.GetTankInventory(tankId);
+    static void Main()
+    {
+        ITankInventoryAdapter fusionAdapter = new TankInventoryFusionAdapter(new FusionAdaptee());
+        ITankInventoryAdapter fdcAdapter = new TankInventoryVeederRootAdapter(new VeederRootAdaptee());
 
-    Console.WriteLine(inventory.Volume < 5
-        ? "Low inventory, sent an alert to the manager"
-        : "Inventory is good, no alert sent");
+        ValidateTankVolume(fusionAdapter);
+
+        ValidateTankVolume(fdcAdapter);
+        return;
+
+        void ValidateTankVolume(ITankInventoryAdapter adapter)
+        {
+            const int tankId = 1;
+            var inventory = adapter.GetTankInventory(tankId);
+
+            Console.WriteLine(inventory.Volume < 5
+                ? "Low inventory, sent an alert to the manager"
+                : "Inventory is good, no alert sent");
+        }
+    }
 }
