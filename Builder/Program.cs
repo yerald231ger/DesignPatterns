54b6ae634b70cce23738749be4ecf3067a0f567b
yerald231ger <yerald231ger@gmail.com>
2025-03-07 01:56:20 -0600

    Add Builder pattern implementation for station creation with validation

---
+++ Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using Builder.Pattern;
+
+var builders = IStationBuilder.GetBuilders();
+
+Console.WriteLine("What kind of station do you want to create? choose a number:");
+builders.ForEach(f => Console.WriteLine($"{f.GetType().Name} -> {builders.IndexOf(f) + 1}"));
+
+var result = int.TryParse(Console.ReadLine(), out var choice) ? choice : 0;
+var reporterFactory = builders.ElementAt(result - 1);
+Console.Clear();
+
+var director = new StationDirector();
+
+Console.WriteLine("Choose a stations director: 1 -> Simple Order Station, 2 -> Random Steps Station");
+var directorChoice = int.TryParse(Console.ReadLine(), out choice) ? choice : 0;
+
+if (directorChoice == 1)
+    director.BuildSimpleOrderStation(reporterFactory);
+else
+    director.BuildRandomStepsStation(reporterFactory);
+
+var station = reporterFactory.Build();
+
+station.DisplayStationInfo();
